e6fd0ee1da7bb1c08f8d783771e1521a08ac69b3
luisda <lusanchezmo@unal.edu.co>
2026-02-17 19:08:39 +0100

    gameplay

--- othello.py
+++ othello.py
@@ -3,21 +3,32 @@
 
 import numpy
 
-def cases_adjacentes_prises(x, y):
-    for i in range(-1, 2):
-        for j in range(-1, 2):
-            if othello_base[x+i][y+j]!=0 and othello_base[x+i][y+j]!=joueur:
-                return True
-    return False
+def positions_capturees(tableau, ligne, col, joueur):
+    if tableau[ligne][col] != 0:
+        return []
+    rival = 1 if joueur == 2 else 2
+    toutes_captures = []
 
-def position_valide(x, y, joueur):
-    if x < 0 or x > 7 and y < 0 or y > 7:
-        return False
-    elif othello_base[x][y] != 0:
-        return False
-    elif not cases_adjacentes_prises(x, y, joueur):
-        return False
-    return True
+    directions = [(-1,-1), (-1,0), (-1,1), (0,-1), (0,1), (1,-1), (1,0), (1,1)]
+    for dl, dc in directions:
+        chemin = []
+        l, c = ligne + dl, col + dc
+
+        while 0 <= l < 8 and 0 <= c < 8 and tableau[l][c] == rival:
+            chemin.append((l,c))
+            l+=dl
+            c+=dc
+        if 0 <= l < 8 and 0 <= c < 8 and tableau[l][c] == joueur:
+            toutes_captures.extend(chemin)
+    return toutes_captures
+
+def mouvements_valides(tableau, joueur):
+    valides = []
+    for i in range(8):
+        for j in range(8):
+            if positions_capturees(tableau, i, j, joueur):
+                valides.append((i,j))
+    return valides
 
 othello_base = numpy.zeros((8, 8), dtype=int)
 othello_base[3][3] = 1
@@ -29,15 +40,25 @@ joueur = 2
 
 while True:
     print(othello_base)
-    x = int(input("Enter the x coordinate"))
-    y = int(input("Enter the y coordinate"))
-    if position_valide(x, y, joueur):
-        othello_base[x][y] = joueur
-        if joueur == 1:
-            joueur = 2
-        else:
-            joueur = 1
-    else:
-        print("Position non valide, re-essayez")
+    mouvements = mouvements_valides(othello_base, joueur)
 
+    if not mouvements:
+        print(f"The player {'Black' if joueur == 2 else 'White'} has no movements")
+        autre_joueur = 1 if joueur == 2 else 2
+        if not mouvements_valides(othello_base, autre_joueur):
+            print("End")
+            break
+        joueur = autre_joueur
+        continue
     
+    print(f"Turn of the: {'Blacks (2)' if joueur == 2 else 'Whites (1)'}")
+    x = int(input("Enter the x coordinate: "))
+    y = int(input("Enter the y coordinate: "))
+    catchs = positions_capturees(othello_base, x, y, joueur)
+    if catchs:
+        othello_base[x][y] = joueur
+        for rl, rc in catchs:
+            othello_base[rl][rc] = joueur
+        joueur = 1 if joueur == 2 else 2 # change le turn
+    else: 
+        print("bad movement")
